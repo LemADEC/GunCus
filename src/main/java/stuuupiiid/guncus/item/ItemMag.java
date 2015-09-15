3ff30cfaea18a32843635584baf648441ec200d2
LemADEC <lemadec.fr@gmail.com>
2015-09-15 22:21:30 +0200

    Added magazine size in its tooltip

--- src/main/java/stuuupiiid/guncus/item/ItemMag.java
+++ src/main/java/stuuupiiid/guncus/item/ItemMag.java
@@ -19,7 +19,19 @@ public class ItemMag extends GunCusItem {
 	}
 	
 	@Override
-	public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par2List, boolean par4) {
-		par2List.add("Pack: " + pack);
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean par4) {
+		ItemStack itemStackBullet = new ItemStack(ItemBullet.bulletsList.get(pack).get(bulletId), 0);
+		int bulletCount = getMaxDamage() - itemStack.getItemDamage();
+		if (bulletCount == getMaxDamage()) {
+			list.add("Full magazine of " + itemStackBullet.getDisplayName());
+			list.add("Bullet count: " + bulletCount + " / " + getMaxDamage());
+		} else if (bulletCount == 0) {
+			list.add("Empty magazine of " + itemStackBullet.getDisplayName());
+			list.add("Use an Ammo box to fill it up.");
+		} else {
+			list.add("Magazine of " + itemStackBullet.getDisplayName());
+			list.add("Bullet count: " + bulletCount + " / " + getMaxDamage());
+		}
+		list.add("Pack: " + pack);
 	}
 }
