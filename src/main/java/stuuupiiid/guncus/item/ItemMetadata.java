c446b36e6db4f82d32ec1408d4e500958e46118f
LemADEC <lemadec.fr@gmail.com>
2015-09-15 04:38:31 +0200

    Fixed gun parts item registration

--- src/main/java/stuuupiiid/guncus/item/ItemMetadata.java
+++ src/main/java/stuuupiiid/guncus/item/ItemMetadata.java
@@ -1,5 +1,6 @@
 package stuuupiiid.guncus.item;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -17,46 +18,48 @@ public class ItemMetadata extends GunCusItem {
 	public CustomizationPart[] metadatas;
 	public IIcon[] icons;
 	public String unlocalized;
-
+	
 	public ItemMetadata(String unlocalized, String iconName, CustomizationPart[] metadatas) {
 		super();
-
+		
 		this.unlocalized = unlocalized;
 		this.iconString = iconName;
 		this.metadatas = metadatas;
 		setHasSubtypes(true);
-
-		for (int v1 = 0; v1 < this.metadatas.length; v1++) {
+		
+		GameRegistry.registerItem(this, unlocalized);
+		
+		for (int v1 = 0; v1 < metadatas.length; v1++) {
 			LanguageRegistry.addName(new ItemStack(this, 1, v1), this.metadatas[v1].localized);
 		}
 	}
-
+	
 	@Override
 	public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, List par3List) {
-		for (int j = 0; j < this.metadatas.length; j++) {
+		for (int j = 0; j < metadatas.length; j++) {
 			ItemStack itemStack = new ItemStack(par1, 1, j);
 			par3List.add(itemStack);
 		}
 	}
-
+	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IIconRegister par1IconRegister) {
-		this.icons = new IIcon[this.metadatas.length];
-
-		for (int i = 0; i < this.metadatas.length; i++) {
-			this.icons[i] = par1IconRegister.registerIcon("guncus:" + this.iconString + i);
+		icons = new IIcon[metadatas.length];
+		
+		for (int i = 0; i < metadatas.length; i++) {
+			icons[i] = par1IconRegister.registerIcon("guncus:" + iconString + i);
 		}
 	}
-
+	
 	@Override
 	@SideOnly(Side.CLIENT)
-	public IIcon getIconFromDamage(int par1) {
-		return this.icons[par1];
+	public IIcon getIconFromDamage(int damage) {
+		return icons[damage];
 	}
-
+	
 	@Override
 	public String getUnlocalizedName(ItemStack par1ItemStack) {
-		return this.unlocalized + par1ItemStack.getItemDamage();
+		return unlocalized + par1ItemStack.getItemDamage();
 	}
 }
